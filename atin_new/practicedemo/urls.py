8db90551bdcf1f95bec8c17d480a7b9799cf7155
Atin Ray <atin.ray@shyamsteel.net>
2025-09-10 22:13:56 +0530

    data added

--- atin_new/practicedemo/urls.py
+++ atin_new/practicedemo/urls.py
@@ -3,5 +3,5 @@ from rest_framework import routers
 from practicedemo import views
 
 urlpatterns = [
-    ('members_atin_api/',views.SelectionMember.as_view())
+    path('members_atin_api/',views.SelectionMember.as_view())
 ]
